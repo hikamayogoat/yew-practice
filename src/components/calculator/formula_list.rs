720d4d41846857dfda06fbb96142da4d627c85ba
hikamayogoat <hikamayo.dw@gmail.com>
2023-01-31 19:25:26 +0900

    表示物のリストをvecで持つようにしてみる

--- src/components/calculator/formula_list.rs
+++ src/components/calculator/formula_list.rs
@@ -1,19 +1,36 @@
-use yew::{function_component, html};
+use yew::{function_component, html, Html};
 use crate::components::calculator::formula::Formula;
 use crate::components::calculator::types::CalcHistory;
 
 #[function_component(FormulaList)]
 pub fn formula_list() -> Html {
-  let calc_history = CalcHistory {
-    id: 1,
-    left: 1,
-    right: 2,
-    answer: 3,
-  };
+  let calc_histories = vec![
+    CalcHistory {
+      id: 1,
+      left: 1,
+      right: 2,
+      answer: 3,
+    },
+    CalcHistory {
+      id: 2,
+      left: 4,
+      right: 5,
+      answer: 9,
+    },
+    CalcHistory {
+      id: 3,
+      left: 6,
+      right: 7,
+      answer: 13,
+    },
+
+  ];
 
   html! {
     <ul class="list-group">
-      <Formula left={calc_history.left} right={calc_history.right} answer={calc_history.answer} />
+      {calc_histories.iter().map(|history| html! {
+        <Formula left={history.left} right={history.right} answer={history.answer} />
+      }).collect::<Html>()}
     </ul>
   }
 }
