6b8090b2f8eb5e338a19541db56d1a978750ed3f
hikamayogoat <hikamayo.dw@gmail.com>
2023-01-31 18:36:20 +0900

    Webページを表示できるようにする

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # yew-practice
+```
+$ cargo install trunk // https://github.com/thedodd/trunk
+
+// Webサーバー起動
+$ trunk serve --open
+```
 
 # 参考にしたページ
-- https://zenn.dev/azukiazusa/articles/rust-base-web-front-fremework-yew 
+- yew の使い方全般
+  - https://zenn.dev/azukiazusa/articles/rust-base-web-front-fremework-yew 
+- index.html で知らなかった部分
+  - IE対策: https://125naroom.com/web/3507
+  - viewport について: https://gmotech.jp/semlabo/seo/blog/howto-viewport/
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    // この中では1つのルート要素しか許されていない
+    // only one root html element is allowed (hint: you can wrap multiple html elements in a fragment `<></>`)
+    html! {
+        <h1>{ "Hello World" }</h1>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // yew v0.20.0 から start_app に名称変更が入りそうなので注意
+    // cf.https://github.com/yewstack/yew/issues/2531
+    yew::start_app::<App>();
 }
